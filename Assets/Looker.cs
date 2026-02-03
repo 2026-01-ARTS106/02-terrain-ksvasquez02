1b0841c187f95ed3c69d23371c7c0c3dfc61299e
K.S. Vasquez <ksvasquez02@gmail.com>
2026-02-03 17:14:46 -0500

    limit camera angle and add scene details

--- Assets/Looker.cs
+++ Assets/Looker.cs
@@ -4,11 +4,14 @@ using UnityEngine.InputSystem;
 public class Looker : MonoBehaviour
 {
     private InputAction ia_look;
+    private const float LOCK_ANGLE = 2.5f;
 
     [SerializeField]
     private float lookSpeed = 180;
 
-    public Vector3 LookDirection { get; set; }
+    private Vector3 lookDirection = Vector3.zero;
+
+    public Vector3 LookDirection { get { return lookDirection; } }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,9 +24,14 @@ public class Looker : MonoBehaviour
     {
         Vector3 angles = transform.eulerAngles;
         Vector2 dir = ia_look.ReadValue<Vector2>();
-        Vector2 rotate = dir * lookSpeed * Time.deltaTime;
+        Vector2 rotate = lookSpeed * Time.deltaTime * dir;
         angles += new Vector3(rotate.y, rotate.x, 0);
-        LookDirection = angles;
-        transform.eulerAngles = angles;
+        lookDirection = angles;
+
+        if (lookDirection.x < 180) lookDirection.x = Mathf.Min(90 - LOCK_ANGLE, lookDirection.x);
+        else if (lookDirection.x > 180) lookDirection.x = Mathf.Max(270 + LOCK_ANGLE, lookDirection.x);
+        else Debug.LogWarning($"Look angle out of bounds: {lookDirection.x}");
+
+        transform.eulerAngles = lookDirection;
     }
 }
